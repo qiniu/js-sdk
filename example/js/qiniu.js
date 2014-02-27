6a9318ce04a60dfc5522a3bd9ccb29fdf1321fcc
SunLn <dengchenhua@gmail.com>
2014-02-27 11:24:09 +0800

    update qinu.js  / change some global func to local

--- example/js/qiniu.js
+++ example/js/qiniu.js
@@ -1,205 +1,5 @@
-var detectIEVersion = function() {
-    var v = 4,
-        div = document.createElement('div'),
-        all = div.getElementsByTagName('i');
-    while (
-        div.innerHTML = '<!--[if gt IE ' + v + ']><i></i><![endif]-->',
-        all[0]
-    ) {
-        v++;
-    }
-    return v > 4 ? v : false;
-};
-
-var isImage = function(url) {
-    var res, suffix = "";
-    var imageSuffixes = ["png", "jpg", "jpeg", "gif", "bmp"];
-    var suffixMatch = /\.([a-zA-Z0-9]+)(\?|\@|$)/;
-
-    if (!url || !suffixMatch.test(url)) {
-        return false;
-    }
-    res = suffixMatch.exec(url);
-    suffix = res[1].toLowerCase();
-    for (var i = 0, l = imageSuffixes.length; i < l; i++) {
-        if (suffix === imageSuffixes[i]) {
-            return true;
-        }
-    }
-    return false;
-};
-var utf8_encode = function(argString) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   improved by: sowberry
-    // +    tweaked by: Jack
-    // +   bugfixed by: Onno Marsman
-    // +   improved by: Yves Sucaet
-    // +   bugfixed by: Onno Marsman
-    // +   bugfixed by: Ulrich
-    // +   bugfixed by: Rafal Kukawski
-    // +   improved by: kirilloid
-    // +   bugfixed by: kirilloid
-    // *     example 1: utf8_encode('Kevin van Zonneveld');
-    // *     returns 1: 'Kevin van Zonneveld'
-
-    if (argString === null || typeof argString === 'undefined') {
-        return '';
-    }
-
-    var string = (argString + ''); // .replace(/\r\n/g, '\n').replace(/\r/g, '\n');
-    var utftext = '',
-        start, end, stringl = 0;
-
-    start = end = 0;
-    stringl = string.length;
-    for (var n = 0; n < stringl; n++) {
-        var c1 = string.charCodeAt(n);
-        var enc = null;
-
-        if (c1 < 128) {
-            end++;
-        } else if (c1 > 127 && c1 < 2048) {
-            enc = String.fromCharCode(
-                (c1 >> 6) | 192, (c1 & 63) | 128
-            );
-        } else if (c1 & 0xF800 ^ 0xD800 > 0) {
-            enc = String.fromCharCode(
-                (c1 >> 12) | 224, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
-            );
-        } else { // surrogate pairs
-            if (c1 & 0xFC00 ^ 0xD800 > 0) {
-                throw new RangeError('Unmatched trail surrogate at ' + n);
-            }
-            var c2 = string.charCodeAt(++n);
-            if (c2 & 0xFC00 ^ 0xDC00 > 0) {
-                throw new RangeError('Unmatched lead surrogate at ' + (n - 1));
-            }
-            c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
-            enc = String.fromCharCode(
-                (c1 >> 18) | 240, ((c1 >> 12) & 63) | 128, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
-            );
-        }
-        if (enc !== null) {
-            if (end > start) {
-                utftext += string.slice(start, end);
-            }
-            utftext += enc;
-            start = end = n + 1;
-        }
-    }
-
-    if (end > start) {
-        utftext += string.slice(start, stringl);
-    }
-
-    return utftext;
-};
-
-var base64_encode = function(data) {
-    // http://kevin.vanzonneveld.net
-    // +   original by: Tyler Akins (http://rumkin.com)
-    // +   improved by: Bayron Guevara
-    // +   improved by: Thunder.m
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // +   bugfixed by: Pellentesque Malesuada
-    // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-    // -    depends on: utf8_encode
-    // *     example 1: base64_encode('Kevin van Zonneveld');
-    // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
-    // mozilla has this native
-    // - but breaks in 2.0.0.12!
-    //if (typeof this.window['atob'] == 'function') {
-    //    return atob(data);
-    //}
-    var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
-    var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
-        ac = 0,
-        enc = '',
-        tmp_arr = [];
-
-    if (!data) {
-        return data;
-    }
-
-    data = utf8_encode(data + '');
-
-    do { // pack three octets into four hexets
-        o1 = data.charCodeAt(i++);
-        o2 = data.charCodeAt(i++);
-        o3 = data.charCodeAt(i++);
-
-        bits = o1 << 16 | o2 << 8 | o3;
-
-        h1 = bits >> 18 & 0x3f;
-        h2 = bits >> 12 & 0x3f;
-        h3 = bits >> 6 & 0x3f;
-        h4 = bits & 0x3f;
-
-        // use hexets to index into b64, and append result to encoded string
-        tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
-    } while (i < data.length);
-
-    enc = tmp_arr.join('');
-
-    switch (data.length % 3) {
-        case 1:
-            enc = enc.slice(0, -2) + '==';
-            break;
-        case 2:
-            enc = enc.slice(0, -1) + '=';
-            break;
-    }
-
-    return enc;
-};
-
-var URLSafeBase64Encode = function(v) {
-    v = base64_encode(v);
-    return v.replace(/\//g, '_').replace(/\+/g, '-');
-};
-
-var createAjax = function(argument) {
-    if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-    } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    return xmlhttp;
-};
-
-var parseJSON = function(data) {
-    // Attempt to parse using the native JSON parser first
-    if (window.JSON && window.JSON.parse) {
-        return window.JSON.parse(data);
-    }
-
-    if (data === null) {
-        return data;
-    }
-    if (typeof data === "string") {
-
-        // Make sure leading/trailing whitespace is removed (IE can't handle it)
-        data = trim(data);
-
-        if (data) {
-            // Make sure the incoming data is actual JSON
-            // Logic borrowed from http://json.org/json2.js
-            if (/^[\],:{}\s]*$/.test(data.replace(/\\(?:["\\\/bfnrt]|u[\da-fA-F]{4})/g, "@").replace(/"[^"\\\r\n]*"|true|false|null|-?(?:\d+\.|)\d+(?:[eE][+-]?\d+|)/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) {
-
-                return (new Function("return " + data))();
-            }
-        }
-    }
-};
-
-var trim = function(text) {
-    return text === null ? "" : trim.call(text);
-};
-
-
-
+/*global plupload */
+/*global ActiveXObject */
 function Qiniu(op) {
     if (!op.uptoken_url || !op.domain) {
         return false;
@@ -215,10 +15,211 @@ function Qiniu(op) {
     var uptoken_url = op.uptoken_url;
     this.domain = op.domain;
 
-    //Todo ie7 handler / parseJson bug;
+    this.detectIEVersion = function() {
+        var v = 4,
+            div = document.createElement('div'),
+            all = div.getElementsByTagName('i');
+        while (
+            div.innerHTML = '<!--[if gt IE ' + v + ']><i></i><![endif]-->',
+            all[0]
+        ) {
+            v++;
+        }
+        return v > 4 ? v : false;
+    };
+
+    this.isImage = function(url) {
+        var res, suffix = "";
+        var imageSuffixes = ["png", "jpg", "jpeg", "gif", "bmp"];
+        var suffixMatch = /\.([a-zA-Z0-9]+)(\?|\@|$)/;
+
+        if (!url || !suffixMatch.test(url)) {
+            return false;
+        }
+        res = suffixMatch.exec(url);
+        suffix = res[1].toLowerCase();
+        for (var i = 0, l = imageSuffixes.length; i < l; i++) {
+            if (suffix === imageSuffixes[i]) {
+                return true;
+            }
+        }
+        return false;
+    };
+    this.utf8_encode = function(argString) {
+        // http://kevin.vanzonneveld.net
+        // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   improved by: sowberry
+        // +    tweaked by: Jack
+        // +   bugfixed by: Onno Marsman
+        // +   improved by: Yves Sucaet
+        // +   bugfixed by: Onno Marsman
+        // +   bugfixed by: Ulrich
+        // +   bugfixed by: Rafal Kukawski
+        // +   improved by: kirilloid
+        // +   bugfixed by: kirilloid
+        // *     example 1: this.utf8_encode('Kevin van Zonneveld');
+        // *     returns 1: 'Kevin van Zonneveld'
+
+        if (argString === null || typeof argString === 'undefined') {
+            return '';
+        }
+
+        var string = (argString + ''); // .replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+        var utftext = '',
+            start, end, stringl = 0;
+
+        start = end = 0;
+        stringl = string.length;
+        for (var n = 0; n < stringl; n++) {
+            var c1 = string.charCodeAt(n);
+            var enc = null;
+
+            if (c1 < 128) {
+                end++;
+            } else if (c1 > 127 && c1 < 2048) {
+                enc = String.fromCharCode(
+                    (c1 >> 6) | 192, (c1 & 63) | 128
+                );
+            } else if (c1 & 0xF800 ^ 0xD800 > 0) {
+                enc = String.fromCharCode(
+                    (c1 >> 12) | 224, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                );
+            } else { // surrogate pairs
+                if (c1 & 0xFC00 ^ 0xD800 > 0) {
+                    throw new RangeError('Unmatched trail surrogate at ' + n);
+                }
+                var c2 = string.charCodeAt(++n);
+                if (c2 & 0xFC00 ^ 0xDC00 > 0) {
+                    throw new RangeError('Unmatched lead surrogate at ' + (n - 1));
+                }
+                c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
+                enc = String.fromCharCode(
+                    (c1 >> 18) | 240, ((c1 >> 12) & 63) | 128, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                );
+            }
+            if (enc !== null) {
+                if (end > start) {
+                    utftext += string.slice(start, end);
+                }
+                utftext += enc;
+                start = end = n + 1;
+            }
+        }
+
+        if (end > start) {
+            utftext += string.slice(start, stringl);
+        }
+
+        return utftext;
+    };
+
+    this.base64_encode = function(data) {
+        // http://kevin.vanzonneveld.net
+        // +   original by: Tyler Akins (http://rumkin.com)
+        // +   improved by: Bayron Guevara
+        // +   improved by: Thunder.m
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // +   bugfixed by: Pellentesque Malesuada
+        // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+        // -    depends on: this.utf8_encode
+        // *     example 1: this.base64_encode('Kevin van Zonneveld');
+        // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+        // mozilla has this native
+        // - but breaks in 2.0.0.12!
+        //if (typeof this.window['atob'] == 'function') {
+        //    return atob(data);
+        //}
+        var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+        var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+            ac = 0,
+            enc = '',
+            tmp_arr = [];
+
+        if (!data) {
+            return data;
+        }
+
+        data = this.utf8_encode(data + '');
+
+        do { // pack three octets into four hexets
+            o1 = data.charCodeAt(i++);
+            o2 = data.charCodeAt(i++);
+            o3 = data.charCodeAt(i++);
+
+            bits = o1 << 16 | o2 << 8 | o3;
+
+            h1 = bits >> 18 & 0x3f;
+            h2 = bits >> 12 & 0x3f;
+            h3 = bits >> 6 & 0x3f;
+            h4 = bits & 0x3f;
+
+            // use hexets to index into b64, and append result to encoded string
+            tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+        } while (i < data.length);
+
+        enc = tmp_arr.join('');
+
+        switch (data.length % 3) {
+            case 1:
+                enc = enc.slice(0, -2) + '==';
+                break;
+            case 2:
+                enc = enc.slice(0, -1) + '=';
+                break;
+        }
+
+        return enc;
+    };
+
+    this.URLSafeBase64Encode = function(v) {
+        v = this.base64_encode(v);
+        return v.replace(/\//g, '_').replace(/\+/g, '-');
+    };
+
+    this.createAjax = function(argument) {
+        var xmlhttp = {};
+        if (window.XMLHttpRequest) {
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        return xmlhttp;
+    };
+
+    this.parseJSON = function(data) {
+        // Attempt to parse using the native JSON parser first
+        if (window.JSON && window.JSON.parse) {
+            return window.JSON.parse(data);
+        }
+
+        if (data === null) {
+            return data;
+        }
+        if (typeof data === "string") {
+
+            // Make sure leading/trailing whitespace is removed (IE can't handle it)
+            data = this.trim(data);
+
+            if (data) {
+                // Make sure the incoming data is actual JSON
+                // Logic borrowed from http://json.org/json2.js
+                if (/^[\],:{}\s]*$/.test(data.replace(/\\(?:["\\\/bfnrt]|u[\da-fA-F]{4})/g, "@").replace(/"[^"\\\r\n]*"|true|false|null|-?(?:\d+\.|)\d+(?:[eE][+-]?\d+|)/g, "]").replace(/(?:^|:|,)(?:\s*\[)+/g, ""))) {
+
+                    return (new Function("return " + data))();
+                }
+            }
+        }
+    };
+
+    this.trim = function(text) {
+        return text === null ? "" : this.trim.call(text);
+    };
+
+    //Todo ie7 handler / this.parseJson bug;
 
 
-    var ie = detectIEVersion();
+    var ie = this.detectIEVersion();
     if (ie && ie <= 9 && op.chunk_size && op.runtimes.indexOf('flash') >= 0) {
         /*
         link: http://www.plupload.com/docs/Frequently-Asked-Questions#when-to-use-chunking-and-when-not
@@ -252,14 +253,14 @@ function Qiniu(op) {
     var uploader = new plupload.Uploader(option);
     this.uploader = uploader;
 
-
+    var that = this;
     var getUpToken = function() {
-        var ajax = createAjax();
+        var ajax = that.createAjax();
         ajax.open('GET', uptoken_url, true);
         ajax.setRequestHeader("If-Modified-Since", "0");
         ajax.onreadystatechange = function() {
             if (ajax.readyState === 4 && ajax.status === 200) {
-                var res = parseJSON(ajax.responseText);
+                var res = that.parseJSON(ajax.responseText);
                 token = res.uptoken;
             }
         };
@@ -270,10 +271,6 @@ function Qiniu(op) {
         getUpToken();
     });
     uploader.init();
-
-    // uploader.Error_Handler = Error_Handler;
-    // uploader.FileUploaded_Handler = FileUploaded_Handler;
-
 
     uploader.bind('FilesAdded', function(up, files) {
         if (up.getOption('auto_start')) {
@@ -323,7 +320,7 @@ function Qiniu(op) {
     });
 
     uploader.bind('ChunkUploaded', function(up, file, info) {
-        var res = parseJSON(info.response);
+        var res = that.parseJSON(info.response);
 
         ctx = ctx ? ctx + ',' + res.ctx : res.ctx;
         var leftSize = info.total - info.offset;
@@ -382,7 +379,7 @@ function Qiniu(op) {
                                 errTip = "未知错误。";
                                 break;
                         }
-                        var errorObj = parseJSON(err.response);
+                        var errorObj = this.parseJSON(err.response);
                         console.log(errorObj);
                         errTip = errTip + '(' + err.status + '：' + errorObj.error + ')';
                         break;
@@ -415,14 +412,14 @@ function Qiniu(op) {
 
     uploader.bind('FileUploaded', (function(FileUploaded_Handler) {
         return function(up, file, info) {
-            var res = parseJSON(info.response);
+            var res = that.parseJSON(info.response);
             // console.log(info.response);
-            // console.log(this === uploader);
+            // console.log(that === uploader);
             ctx = ctx ? ctx : res.ctx;
             // console.log('FileUploaded_Handler', uploader.Error_Handler);
             if (ctx) {
-                var url = 'http://up.qiniu.com/mkfile/' + file.size + '/key/' + URLSafeBase64Encode(file.name);
-                var ajax = createAjax();
+                var url = 'http://up.qiniu.com/mkfile/' + file.size + '/key/' + that.URLSafeBase64Encode(file.name);
+                var ajax = that.createAjax();
                 ajax.open('POST', url, true);
                 ajax.setRequestHeader('Content-Type', 'text/plain;charset=UTF-8');
                 ajax.setRequestHeader('Authorization', 'UpToken ' + token);
@@ -450,9 +447,6 @@ function Qiniu(op) {
 }
 
 
-/* global URLSafeBase64Encode */
-/* global Qiniu */
-/* global createAjax */
 
 Qiniu.prototype.getUrl = function(key) {
     if (!key) {
@@ -509,11 +503,11 @@ Qiniu.prototype.imageMogr2 = function(op, key) {
         crop = op.crop || '',
         quality = op.quality || '',
         rotate = op.rotate || '',
-        format = op.format || '';
-    blur = op.blur || '';
+        format = op.format || '',
+        blur = op.blur || '';
     //Todo check option
 
-    var imageUrl = 'imageMogr2'
+    var imageUrl = 'imageMogr2';
 
     imageUrl += auto_orient ? '/auto-orient' : '';
     imageUrl += thumbnail ? '/thumbnail/' + thumbnail : '';
@@ -545,7 +539,7 @@ Qiniu.prototype.watermark = function(op, key) {
         if (!image) {
             return false;
         }
-        imageUrl += image ? '/image/' + URLSafeBase64Encode(image) : '';
+        imageUrl += image ? '/image/' + this.URLSafeBase64Encode(image) : '';
     } else if (mode === 2) {
         var text = op.text ? op.text : '',
             font = op.font ? op.font : '',
@@ -554,10 +548,10 @@ Qiniu.prototype.watermark = function(op, key) {
         if (!text) {
             return false;
         }
-        imageUrl += text ? '/text/' + URLSafeBase64Encode(text) : '';
-        imageUrl += font ? '/font/' + URLSafeBase64Encode(font) : '';
+        imageUrl += text ? '/text/' + this.URLSafeBase64Encode(text) : '';
+        imageUrl += font ? '/font/' + this.URLSafeBase64Encode(font) : '';
         imageUrl += fontsize ? '/fontsize/' + fontsize : '';
-        imageUrl += fill ? '/fill/' + URLSafeBase64Encode(fill) : '';
+        imageUrl += fill ? '/fill/' + this.URLSafeBase64Encode(fill) : '';
     } else {
         // Todo mode3
         return false;
@@ -585,12 +579,13 @@ Qiniu.prototype.imageInfo = function(key) {
         return false;
     }
     var url = this.getUrl(key) + '?imageInfo';
-    var xhr = createAjax();
+    var xhr = this.createAjax();
     var info;
+    var that = this;
     xhr.open('GET', url, false);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            info = $.parseJSON(xhr.responseText);
+            info = that.parseJSON(xhr.responseText);
         }
     };
     xhr.send();
@@ -603,12 +598,13 @@ Qiniu.prototype.exif = function(key) {
         return false;
     }
     var url = this.getUrl(key) + '?exif';
-    var xhr = createAjax();
+    var xhr = this.createAjax();
     var info;
+    var that = this;
     xhr.open('GET', url, false);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
-            info = $.parseJSON(xhr.responseText);
+            info = that.parseJSON(xhr.responseText);
         }
     };
     xhr.send();
