8af893ba78e6161cbdda42208aecae8baa889cda
SunLn <dengchenhua@gmail.com>
2014-03-10 14:11:53 +0800

    revert confi.js

--- demo/config.js
+++ demo/config.js
@@ -1,14 +1,6 @@
-// module.exports = {
-//     'ACCESS_KEY': '<Your Access Key>',
-//     'SECRET_KEY': '<Your Secret Key>',
-//     'Bucket_Name': '<Your Bucket Name>',
-//     'Port': 18080
-// }
-
-
 module.exports = {
-    'ACCESS_KEY': '0MLvWPnyya1WtPnXFy9KLyGHyFPNdZceomLVk0c9',
-    'SECRET_KEY': 'o5itRgrXxoD6XQ5wDWKQ7h--eWvWyQVKcsIURuEV',
-    'Bucket_Name': 'qiniu-plupload',
+    'ACCESS_KEY': '<Your Access Key>',
+    'SECRET_KEY': '<Your Secret Key>',
+    'Bucket_Name': '<Your Bucket Name>',
     'Port': 18080
 }
