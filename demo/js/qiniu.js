81cb7de40e39b4ed3fe89f041f2d17688a843ae0
SunLn <dengchenhua@gmail.com>
2014-04-03 17:16:04 +0800

    KeyHandler -> Key

--- demo/js/qiniu.js
+++ demo/js/qiniu.js
@@ -240,7 +240,7 @@ function QiniuJsSDK() {
 
         that.uptoken_url = op.uptoken_url;
         that.token = '';
-        that.key_handler = typeof op.init.KeyHandler === 'function' ? op.init.KeyHandler : '';
+        that.key_handler = typeof op.init.Key === 'function' ? op.init.Key : '';
         this.domain = op.domain;
         var ctx = '';
 
