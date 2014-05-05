a553bd48c160876db943e9b04a2dea7ea5d50c2f
SunLn <dengchenhua@gmail.com>
2014-05-05 16:37:19 +0800

    extract config to backend

--- demo/config.js
+++ demo/config.js
@@ -2,5 +2,7 @@ module.exports = {
     'ACCESS_KEY': '<Your Access Key>',
     'SECRET_KEY': '<Your Secret Key>',
     'Bucket_Name': '<Your Bucket Name>',
-    'Port': 18080
+    'Port': 18080,
+    'Uptoken_Url': '/token',
+    'Domain': 'http://qiniu-plupload.qiniudn.com/'
 }
