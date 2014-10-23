8b6030eb90056f448af9c24df2ea47596753aa17
Mc.Spring <Heresy.Mc@gmail.com>
2014-10-23 15:09:16 +0800

    Update config.js
    change default port to 19110

--- demo/config.js
+++ demo/config.js
@@ -2,7 +2,7 @@ module.exports = {
     'ACCESS_KEY': '<Your Access Key>',
     'SECRET_KEY': '<Your Secret Key>',
     'Bucket_Name': '<Your Bucket Name>',
-    'Port': 18080,
+    'Port': 19110,
     'Uptoken_Url': '/uptoken',
     'Domain': 'http://qiniu-plupload.qiniudn.com/'
 };
