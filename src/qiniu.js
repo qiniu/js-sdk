c75bdfee9f2c59ae9282881b391336efc95a1027
SunLn <dengchenhua@gmail.com>
2014-12-27 22:28:50 +0800

    re-replace base64_encode

--- src/qiniu.js
+++ src/qiniu.js
@@ -52,10 +52,140 @@ function QiniuJsSDK(op) {
             }
             return xmlhttp;
         },
-        URLSafeBase64Encode: function(v) {
-            v = mOxie.btoa(v);
+        // url_safe_base64_encode: function(v) {
+        //     v = mOxie.btoa(v);
+        //     return v.replace(/\//g, '_').replace(/\+/g, '-');
+        // },
+        utf8_encode: function(argString) {
+            // http://kevin.vanzonneveld.net
+            // +   original by: Webtoolkit.info (http://www.webtoolkit.info/)
+            // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+            // +   improved by: sowberry
+            // +    tweaked by: Jack
+            // +   bugfixed by: Onno Marsman
+            // +   improved by: Yves Sucaet
+            // +   bugfixed by: Onno Marsman
+            // +   bugfixed by: Ulrich
+            // +   bugfixed by: Rafal Kukawski
+            // +   improved by: kirilloid
+            // +   bugfixed by: kirilloid
+            // *     example 1: this.utf8_encode('Kevin van Zonneveld');
+            // *     returns 1: 'Kevin van Zonneveld'
+
+            if (argString === null || typeof argString === 'undefined') {
+                return '';
+            }
+
+            var string = (argString + ''); // .replace(/\r\n/g, '\n').replace(/\r/g, '\n');
+            var utftext = '',
+                start, end, stringl = 0;
+
+            start = end = 0;
+            stringl = string.length;
+            for (var n = 0; n < stringl; n++) {
+                var c1 = string.charCodeAt(n);
+                var enc = null;
+
+                if (c1 < 128) {
+                    end++;
+                } else if (c1 > 127 && c1 < 2048) {
+                    enc = String.fromCharCode(
+                        (c1 >> 6) | 192, (c1 & 63) | 128
+                    );
+                } else if (c1 & 0xF800 ^ 0xD800 > 0) {
+                    enc = String.fromCharCode(
+                        (c1 >> 12) | 224, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                    );
+                } else { // surrogate pairs
+                    if (c1 & 0xFC00 ^ 0xD800 > 0) {
+                        throw new RangeError('Unmatched trail surrogate at ' + n);
+                    }
+                    var c2 = string.charCodeAt(++n);
+                    if (c2 & 0xFC00 ^ 0xDC00 > 0) {
+                        throw new RangeError('Unmatched lead surrogate at ' + (n - 1));
+                    }
+                    c1 = ((c1 & 0x3FF) << 10) + (c2 & 0x3FF) + 0x10000;
+                    enc = String.fromCharCode(
+                        (c1 >> 18) | 240, ((c1 >> 12) & 63) | 128, ((c1 >> 6) & 63) | 128, (c1 & 63) | 128
+                    );
+                }
+                if (enc !== null) {
+                    if (end > start) {
+                        utftext += string.slice(start, end);
+                    }
+                    utftext += enc;
+                    start = end = n + 1;
+                }
+            }
+
+            if (end > start) {
+                utftext += string.slice(start, stringl);
+            }
+
+            return utftext;
+        },
+        base64_encode: function(data) {
+            // http://kevin.vanzonneveld.net
+            // +   original by: Tyler Akins (http://rumkin.com)
+            // +   improved by: Bayron Guevara
+            // +   improved by: Thunder.m
+            // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+            // +   bugfixed by: Pellentesque Malesuada
+            // +   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+            // -    depends on: this.utf8_encode
+            // *     example 1: this.base64_encode('Kevin van Zonneveld');
+            // *     returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+            // mozilla has this native
+            // - but breaks in 2.0.0.12!
+            //if (typeof this.window['atob'] == 'function') {
+            //    return atob(data);
+            //}
+            var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+                ac = 0,
+                enc = '',
+                tmp_arr = [];
+
+            if (!data) {
+                return data;
+            }
+
+            data = this.utf8_encode(data + '');
+
+            do { // pack three octets into four hexets
+                o1 = data.charCodeAt(i++);
+                o2 = data.charCodeAt(i++);
+                o3 = data.charCodeAt(i++);
+
+                bits = o1 << 16 | o2 << 8 | o3;
+
+                h1 = bits >> 18 & 0x3f;
+                h2 = bits >> 12 & 0x3f;
+                h3 = bits >> 6 & 0x3f;
+                h4 = bits & 0x3f;
+
+                // use hexets to index into b64, and append result to encoded string
+                tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+            } while (i < data.length);
+
+            enc = tmp_arr.join('');
+
+            switch (data.length % 3) {
+                case 1:
+                    enc = enc.slice(0, -2) + '==';
+                    break;
+                case 2:
+                    enc = enc.slice(0, -1) + '=';
+                    break;
+            }
+
+            return enc;
+        },
+        url_safe_base64_encode: function(v) {
+            v = this.base64_encode(v);
             return v.replace(/\//g, '_').replace(/\+/g, '-');
         }
+
     };
 
     var constant = {
@@ -78,7 +208,7 @@ function QiniuJsSDK(op) {
             if (typeof op.init === 'object' && typeof op.init.Key === 'function') {
                 return op.init.Key;
             }
-            return function() {};
+            return null;
         })(),
         file_uploaded_hanlder = (function() {
             if (typeof op.init === 'object' && typeof op.init.FileUploaded === 'function') {
@@ -404,7 +534,7 @@ function QiniuJsSDK(op) {
             var key = '';
             if (!op.save_key) {
                 key = getFileKey(up, file, key_handler);
-                key = key ? '/key/' + util.URLSafeBase64Encode(key) : '';
+                key = key ? '/key/' + util.url_safe_base64_encode(key) : '';
             }
 
             var x_vars_url = getXVarsURL();
@@ -440,9 +570,9 @@ function QiniuJsSDK(op) {
                 for (var x_key in x_vars) {
                     if (x_vars.hasOwnProperty(x_key)) {
                         if (typeof x_vars[x_key] === 'function') {
-                            x_val = util.URLSafeBase64Encode(x_vars[x_key](up, file));
+                            x_val = util.url_safe_base64_encode(x_vars[x_key](up, file));
                         } else if (typeof x_vars[x_key] !== 'object') {
-                            x_val = util.URLSafeBase64Encode(x_vars[x_key]);
+                            x_val = util.url_safe_base64_encode(x_vars[x_key]);
                         }
                         x_vars_url += '/x:' + x_key + '/' + x_val;
                     }
@@ -467,7 +597,7 @@ function QiniuJsSDK(op) {
                             try {
                                 res_downtoken = util.parseJSON(ajax_downtoken.responseText);
                             } catch (e) {
-                                throw ('invalid json format');
+                                throw ('服务端返回了无效');
                             }
 
                             plupload.extend(infoObj, res_downtoken);
