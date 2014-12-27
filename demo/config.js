86bc5ff732666a6c72da05b5b0585099785fbe38
SunLn <dengchenhua@gmail.com>
2014-12-27 12:17:40 +0800

    I really don\'t want Git to track this anymore

--- demo/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'ACCESS_KEY': '<Your Access Key>',
-    'SECRET_KEY': '<Your Secret Key>',
-    'Bucket_Name': '<Your Bucket Name>',
-    'Port': 19110,
-    'Uptoken_Url': '/uptoken',
-    'Domain': 'http://qiniu-plupload.qiniudn.com/'
-};
