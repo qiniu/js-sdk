31d650172b7e9449fc0eac093c2f35c324020e78
SunLn <dengchenhua@gmail.com>
2015-01-18 16:20:19 +0800

    refact

---
+++ demo/server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'ACCESS_KEY': '0MLvWPnyya1WtPnXFy9KLyGHyFPNdZceomLVk0c9',
+    'SECRET_KEY': 'o5itRgrXxoD6XQ5wDWKQ7h--eWvWyQVKcsIURuEV',
+    'Bucket_Name': 'qiniu-plupload',
+    'Port': 19110,
+    'Uptoken_Url': '/uptoken',
+    'Domain': 'http://7b1fyj.com1.z0.glb.clouddn.com/'
+};
