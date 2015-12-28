aa85ffc1ccb488bf15b1e68e0fbfaca28fd99063
codedogfish <jackcool1989@outlook.com>
2015-12-28 11:54:22 +0800

    Make localStorage fallback with cookie
    - Use cookie to store upload file info when browser is not support localStorage
    - Fix get_new_token mistake in Init Event

--- src/qiniu.js
+++ src/qiniu.js
@@ -16,106 +16,49 @@
 
 ;(function( global ){
 
-// var store = function store(key, value) {
-//     var lsSupport = false,
-//         data = null;
-    
-//     // Check for native support
-//     if (localStorage) {
-//         lsSupport = true;
-//     }
-    
-//     // If value is detected, set new or modify store
-//     if (typeof value !== "undefined" && value !== null) {
-//         // Convert object values to JSON
-//         if ( typeof value === 'object' ) {
-//             value = JSON.stringify(value);
-//         }
-//         // Set the store
-//         if (lsSupport) { // Native support
-//             localStorage.setItem(key, value);
-//         } else { // Use Cookie
-//             createCookie(key, value, 30);
-//         }
-//     }
-    
-//     // No value supplied, return value
-//     if (typeof value === "undefined") {
-//         // Get value
-//         if (lsSupport) { // Native support
-//             data = localStorage.getItem(key);
-//         } else { // Use cookie 
-//             data = readCookie(key);
-//         }
-        
-//         // Try to parse JSON...
-//         try {
-//            data = JSON.parse(data);
-//         }
-//         catch(e) {
-//            data = data;
-//         }
-        
-//         return data;
-        
-//     }
-    
-//     // Null specified, remove store
-//     if (value === null) {
-//         if (lsSupport) { // Native support
-//             localStorage.removeItem(key);
-//         } else { // Use cookie
-//             createCookie(key, '', -1);
-//         }
-//     }
-    
-//     /**
-//      * Creates new cookie or removes cookie with negative expiration
-//      * @param  key       The key or identifier for the store
-//      * @param  value     Contents of the store
-//      * @param  exp       Expiration - creation defaults to 30 days
-//      */
-    
-//     function createCookie(key, value, exp) {
-//         var date = new Date();
-//         date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
-//         var expires = "; expires=" + date.toGMTString();
-//         document.cookie = key + "=" + value + expires + "; path=/";
-//     }
-    
-//     /**
-//      * Returns contents of cookie
-//      * @param  key       The key or identifier for the store
-//      */
-    
-//     function readCookie(key) {
-//         var nameEQ = key + "=";
-//         var ca = document.cookie.split(';');
-//         for (var i = 0, max = ca.length; i < max; i++) {
-//             var c = ca[i];
-//             while (c.charAt(0) === ' ') {
-//                 c = c.substring(1, c.length);
-//             }
-//             if (c.indexOf(nameEQ) === 0) {
-//                 return c.substring(nameEQ.length, c.length);
-//             }
-//         }
-//         return null;
-//     }
-    
-// };
+/**
+ * Creates new cookie or removes cookie with negative expiration
+ * @param  key       The key or identifier for the store
+ * @param  value     Contents of the store
+ * @param  exp       Expiration - creation defaults to 30 days
+ */
+function createCookie(key, value, exp) {
+    var date = new Date();
+    date.setTime(date.getTime() + (exp * 24 * 60 * 60 * 1000));
+    var expires = "; expires=" + date.toGMTString();
+    document.cookie = key + "=" + value + expires + "; path=/";
+}
+
+/**
+ * Returns contents of cookie
+ * @param  key       The key or identifier for the store
+ */
+function readCookie(key) {
+    var nameEQ = key + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0, max = ca.length; i < max; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
+        }
+    }
+    return null;
+}
+
 
 if ( !window.localStorage ) {
     window.localStorage = {
-        data: {},
         setItem: function (key, value) {
-            localStorage.data[key] = value;
+            createCookie(key, value, 30);
         },
         getItem: function (key) {
-            return localStorage.data[key] || null ;
+            return readCookie(key);
         },
         removeItem: function (key) {
-            localStorage.data[key] = null;
+            createCookie(key, '', -1);
         }
     };
 }
@@ -651,6 +594,13 @@ function QiniuJsSDK() {
         // bind getUpToken to 'Init' event
         uploader.bind('Init', function(up, params) {
             logger.debug("Init event activated");
+            // if op.get_new_uptoken is not true
+            //      invoke getUptoken when uploader init
+            // else 
+            //      getUptoken everytime before a new file upload
+            if(!op.get_new_uptoken){
+                getUpToken();
+            }
             getUpToken();
         });
 
