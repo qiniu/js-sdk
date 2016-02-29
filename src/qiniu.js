a65cc4e6878e220fa0ec1c2836765d7b347aac7d
defcc <defcc@users.noreply.github.com>
2016-02-29 21:59:37 +0800

    优化 isImage 函数

--- src/qiniu.js
+++ src/qiniu.js
@@ -182,21 +182,7 @@ function QiniuJsSDK() {
      * @return {Boolean} file is a image or not
      */
     this.isImage = function(url) {
-        var res, suffix = "";
-        var imageSuffixes = ["png", "jpg", "jpeg", "gif", "bmp"];
-        var suffixMatch = /\.([a-zA-Z0-9]+)(\?|\@|$)/;
-
-        if (!url || !suffixMatch.test(url)) {
-            return false;
-        }
-        res = suffixMatch.exec(url);
-        suffix = res[1].toLowerCase();
-        for (var i = 0, l = imageSuffixes.length; i < l; i++) {
-            if (suffix === imageSuffixes[i]) {
-                return true;
-            }
-        }
-        return false;
+        return /\.(png|jpg|jpeg|gif|bmp)$/i.test(url);
     };
 
     /**
