6db9d10527ecc8b77af0e9976b8fb9d7caa80ead
taoqf <tao_qiufeng@126.com>
2018-01-20 09:28:05 +0800

    add callback FilesRemoved

--- types/qiniu.d.ts
+++ types/qiniu.d.ts
@@ -54,6 +54,8 @@ declare namespace Qiniu {
 		init: Partial<{
 			// 文件添加进队列后,处理相关的事情
 			FilesAdded(up: plupload, files: File[]): void;
+			// 删除文件事件
+			FilesRemoved(up: plupload, files: File[]): void;
 			// 每个文件上传前,处理相关的事情
 			BeforeUpload(up: plupload, file: File): void;
 			// 每个文件上传时,处理相关的事情
