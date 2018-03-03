e4ef37c30a3e0c03695ac7fd29de71191307195d
XiaoLe <youngleocn@qq.com>
2018-03-03 14:50:14 +0800

    fix bug

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,13 +4,13 @@ var webpack = require("webpack");
 var common = require("./webpack.common.js");
 var OpenBrowserPlugin = require("open-browser-webpack-plugin");
 module.exports = merge(common, {
-  plugins:[new OpenBrowserPlugin({ url: 'http://0.0.0.0:8080/test/demo1/' })],
+  plugins:[new OpenBrowserPlugin({ url: 'http://localhost:8080/test/demo1/' })],
   devServer: {
     disableHostCheck: true,
     progress: true,
     proxy: {
       "/api/*": {
-        target: "http://0.0.0.0:9000",
+        target: "http://localhost:9000/",
         changeOrigin: true,
         secure: false
       }
