c733c8ace5ffec0c9ba33ed8cc25683234729831
winddies <517817136@qq.com>
2020-03-11 19:02:13 +0800

    rename statistic

--- src/statisticsLog.ts
+++ src/statisticsLog.ts
@@ -9,19 +9,19 @@ export class StatisticsLogger{
   }
 
   send(logString, token, retryCount) {
-    let xhr = createXHR();
-    let self = this;
-    xhr.open("POST", "https://uplog.qbox.me/log/3");
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.setRequestHeader("Authorization", "UpToken " + token);
+    let xhr = createXHR()
+    let self = this
+    xhr.open("POST", "https://uplog.qbox.me/log/3")
+    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+    xhr.setRequestHeader("Authorization", "UpToken " + token)
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4) {
         if (xhr.status !== 200) {
-          ++retryCount <= 3 && self.send(logString, token, retryCount);
+          ++retryCount <= 3 && self.send(logString, token, retryCount)
         }
       }
-    };
-    xhr.send(logString);
+    }
+    xhr.send(logString)
   }
 
 }
