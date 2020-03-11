a653d51ead131c9898292a1d7cbcedd66d321e87
winddies <517817136@qq.com>
2020-03-11 18:53:29 +0800

    pool change

--- src/pool.js
+++ src/pool.js
@@ -1,38 +1,52 @@
+import { IChunkInfo } from './upload'
+
+export type Task = (chunkInfo: IChunkInfo) => Promise<void>
+export type QueueContent = {
+  task: IChunkInfo
+  resolve: <T>(value?: T | PromiseLike<T>) => void
+  reject: (reason?: any) => void
+}
+
 export class Pool {
-  constructor(runTask, limit) {
-    this.runTask = runTask;
-    this.queue = [];
-    this.processing = [];
-    this.limit = limit;
+  private runTask: Task
+  private queue: QueueContent[] = []
+  private processing: QueueContent[] = []
+  private limit: number
+
+  constructor(runTask: Task, limit: number) {
+    this.runTask = runTask
+    this.limit = limit
   }
 
-  enqueue(task){
+  enqueue(task: IChunkInfo) {
     return new Promise((resolve, reject) => {
       this.queue.push({
         task,
         resolve,
         reject
-      });
-      this.check();
-    });
+      })
+      this.check()
+    })
   }
-  run(item){
-    this.queue = this.queue.filter(v => v !== item);
-    this.processing.push(item);
+
+  run(item: QueueContent) {
+    this.queue = this.queue.filter(v => v !== item)
+    this.processing.push(item)
     this.runTask(item.task).then(
       () => {
-        this.processing = this.processing.filter(v => v !== item);
-        item.resolve();
-        this.check();
+        this.processing = this.processing.filter(v => v !== item)
+        item.resolve()
+        this.check()
       },
       (err) => item.reject(err)
-    );
+    )
   }
-  check(){
-    let processingNum = this.processing.length;
-    let availableNum = this.limit - processingNum;
-    this.queue.slice(0, availableNum).forEach((item, index) => {
-      this.run(item);
-    });
+
+  check() {
+    const processingNum = this.processing.length
+    const availableNum = this.limit - processingNum
+    this.queue.slice(0, availableNum).forEach(item => {
+      this.run(item)
+    })
   }
 }
