cfec5d50e77239621b6d75510f17a6619c84bbbf
winddies <517817136@qq.com>
2020-07-22 18:51:12 +0800

    add unit test

--- src/__tests__/api.test.ts
+++ src/__tests__/api.test.ts
@@ -1,4 +1,4 @@
-import { getUploadUrl, getUpHosts } from '../api'
+import { getUploadUrl } from '../api'
 import { DEFAULT_CHUNK_SIZE, Config } from '../upload/base'
 import { region } from '../config'
 
@@ -13,24 +13,22 @@ describe('api function test', () => {
       upprotocol: 'https:',
       forceDirect: false,
       chunkSize: DEFAULT_CHUNK_SIZE,
-      concurrentRequestLimit: 3,
+      concurrentRequestLimit: 3
     }
 
     let url: string
-
-    url = await getUploadUrl(config, 'token')
-    expect(getUpHosts).toBeCalled()
+    const token = '4TUDv7mcubtP5yWqzrZiI0YEVLSLEu3NlVvCxT-D:UUEt7wiIOSlS7AoCx9w_pABEFtI=:eyJkZWxldGVBZnRlckRheXMiOjEsInJldHVybkJvZHkiOiJ7XCJrZXlcIjpcIiQoa2V5KVwiLFwiaGFzaFwiOlwiJChldGFnKVwiLFwiZnNpemVcIjokKGZzaXplKSxcImJ1Y2tldFwiOlwiJChidWNrZXQpXCIsXCJuYW1lXCI6XCIkKHg6bmFtZSlcIn0iLCJzY29wZSI6ImtpbmRvbSIsImRlYWRsaW5lIjoxNTk1NDEzNjQxfQ=='
 
     config.region = region.z0
-    url = await getUploadUrl(config, 'token')
+    url = await getUploadUrl(config, token)
     expect(url).toBe('https://upload.qiniup.com')
 
     config.upprotocol = 'http:'
-    url = await getUploadUrl(config, 'token')
+    url = await getUploadUrl(config, token)
     expect(url).toBe('http://upload.qiniup.com')
 
     config.uphost = 'qiniu.com'
-    url = await getUploadUrl(config, 'token')
+    url = await getUploadUrl(config, token)
     expect(url).toBe('http://qiniu.com')
   })
 })
