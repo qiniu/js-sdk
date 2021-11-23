f2d3d18f2eb13fdea3ceb29d8be8d97436ac1a01
yinxulai <yinxulai@hotmail.com>
2021-11-23 10:00:41 +0800

    添加文档、上报问题、 v2 版本的底部导航

--- site/src/components/Layout/index.tsx
+++ site/src/components/Layout/index.tsx
@@ -3,6 +3,51 @@ import * as React from 'react'
 import qiniuLogo from './assets/logo.svg'
 import classnames from './style.less'
 
+
+function OfficialSite() {
+  return (
+    <>
+      © {new Date().getFullYear()} <a href="https://www.qiniu.com/" rel="noopener" target="_blank">七牛云</a>
+    </>
+  )
+}
+
+function Doc() {
+  return (
+    <a
+      rel="noopener"
+      target="_blank"
+      href="http://developer.qiniu.com/code/v6/sdk/javascript.html"
+    >
+      官方文档
+    </a>
+  )
+}
+
+function Issue() {
+  return (
+    <a
+      rel="noopener"
+      target="_blank"
+      href="https://github.com/qiniu/js-sdk/issues"
+    >
+      上报问题
+    </a>
+  )
+}
+
+function V2Href() {
+  return (
+    <a
+      rel="noopener"
+      target="_blank"
+      href="http://jssdk-v2.demo.qiniu.io"
+    >
+      V2版本
+    </a>
+  )
+}
+
 interface IProps { }
 
 export function Layout(props: React.PropsWithChildren<IProps>) {
@@ -14,7 +59,10 @@ export function Layout(props: React.PropsWithChildren<IProps>) {
       </div>
       <footer className={classnames.footer}>
         对象存储文件上传 DEMO
-        © {new Date().getFullYear()} <a href="https://www.qiniu.com/" rel="noopener" target="_blank" >七牛云</a>
+        <OfficialSite />
+        <Doc />
+        <Issue />
+        <V2Href />
       </footer>
     </>
   )
